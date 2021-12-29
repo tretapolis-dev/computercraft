43094db7c4082e4c69199ca9a7343bdbf5d2ccd8
tretapolis-dev <54849610+tretapolis-dev@users.noreply.github.com>
2021-12-29 15:15:16 +0100

    init

---
+++ Quarry.lua
@@ -0,0 +1,17 @@
+print("Quarry by TRETAPOLIS")
+length = 5
+x = 0
+y = 0
+z = 0
+write("Länge x Breite (Blöcke): [5] ")
+local input = read()
+if input ~= "" then length = input end
+
+for currentLength = 1, length, 1 do
+    x=x+1
+    for currentWidht = 1, length, 1 do
+y=y-1
+    
+    end
+
+end
---
+++ Updater.lua
@@ -0,0 +1,4 @@
+shell.run("delete test")
+shell.run("pastebin get ERASvyiY test")
+shell.run("clear")
+shell.run("test")
