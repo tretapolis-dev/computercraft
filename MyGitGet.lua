8489aed14fa1de1c67ac43f3349be08371a7a7b3
tretapolis <MY_NAME@example.com>
2021-12-29 16:13:06 +0100

    December 29, 2021 4:13 PM

--- MyGitGet.lua
+++ MyGitGet.lua
@@ -1,69 +1,25 @@
--- Settings
-local settings_file = shell.resolve(".gd.cfg")
-
-local function getRepo()
-    if fs.exists(settings_file) then
-        local h = fs.open(settings_file, 'r')
-        local s = h.readAll()
-        h.close()
-        return s
-    else
-        print("Repositpory not set.")
-        print("Please specify the base url for all raw files:")
-        write("> ")
-        local r = read()
-        local h = fs.open(settings_file, 'w')
-        h.write(r)
-        h.close()
-        return r
-    end
-end
-
-local repo = getRepo()
-
--- Main
-local path, name = ...
-
-if repo == nil then setRepo() end
-
-while path == nil or string.len(path) == 0 do
-    print("Please specify a file from the repo to download.")
-    write("> ")
-    path = read()
-end
-
-path = string.gsub(path, "\\", "/")
-
-if name == nil or string.len(name) == 0 then
-    print("Please specify a name for the programm (default: startup)")
-    write("> ")
-    name = read()
-    if name == nil or string.len(name) == 0 then name = "startup" end
-end
-name = string.gsub(name, ".lua", " ")
-local file_name = shell.resolve(name)
-
-if fs.exists(file_name) then
-    print("Programm " .. name .. " already exists. Override? (Y/N)")
-    local y = read()
-    if not (y == "y" or y == "Y" or y == "") then return end
-end
-
-print("Downloading " .. path .. "...")
-http.request(repo .. path)
+-- settings
+repo = "https://raw.githubusercontent.com/tretapolis-dev/computercraft/main/"
+name = "test"
+erfolg = true
+-- -
+print("Downloading " .. name .. ".lua ...")
+http.request(repo .. name .. ".lua")
 local loop = true
 while loop do
     local event, url, h = os.pullEvent()
     if event == "http_success" then
         local content = h.readAll()
         h.close()
-        local file = fs.open(file_name, 'w')
+        local file = fs.open(name, 'w')
         file.write(content)
         file.close()
-        print("Saved " .. name)
+        print("Saved " .. name .. ".lua")
         loop = false
     elseif event == "http_failure" then
-        print("Download failed!")
+        print("Download fehlgeschlagen!")
         loop = false
+        erfolg = false
     end
 end
+if erfolg == true then shell.run(name) end
