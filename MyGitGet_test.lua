45e1cde05402c8ed9a8947b4b9d11e994e63e238
tretapolis <MY_NAME@example.com>
2021-12-30 03:55:48 +0100

    December 30, 2021 3:55 AM

--- MyGitGet_test.lua
+++ MyGitGet_test.lua
@@ -22,10 +22,13 @@ name = "test"
 --         -- print("Download fehlgeschlagen!")
 --     end
 -- end
-math2 = math.random(1, 50000)
-request = http.get(repo .. name .. ".lua?t=" .. os.time() .. math2)
-print(math2)
-print(request.readAll())
-request.close()
+-- math2 = math.random(1, 50000)
+-- request = http.get(repo .. name .. ".lua?t=" .. os.time() .. math2)
+-- print(math2)
+-- print(request.readAll())
+-- request.close()
 local cacheBuster = ("%x"):format(math.random(0, 2 ^ 30))
 local response, err = http.get(repo .. name .. ".lua?cb=" .. cacheBuster)
+local sResponse = response.readAll()
+response.close()
+print(sResponse)
