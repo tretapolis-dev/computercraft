7327a41c954b09e43e145d670c4e673bb327981e
tretapolis <MY_NAME@example.com>
2021-12-30 03:17:48 +0100

    December 30, 2021 3:17 AM

--- MyGitGet_test.lua
+++ MyGitGet_test.lua
@@ -1,24 +1,27 @@
--- pastebin get 9wFXXLWx testpull
-shell.run("clear")
--- settings
+-- -- pastebin get 9wFXXLWx testpull
+-- shell.run("clear")
+-- -- settings
 repo = "https://raw.githubusercontent.com/tretapolis-dev/computercraft/main/"
 name = "test"
-if fs.exists(name) == true then fs.delete(name) end
--- -
--- print("Downloading " .. name .. ".lua ...")
-http.request(repo .. name .. ".lua", nil, {os.time()})
-while true do
-    local event, url, h = os.pullEvent()
-    if event == "http_success" then
-        local content = h.readAll()
-        h.close()
-        local file = fs.open(name, 'w')
-        file.write(content)
-        file.close()
-        -- print("Saved " .. name .. ".lua")
-        -- http.request(repo .. name .. ".lua")
-        shell.run(name)
-    elseif event == "http_failure" then
-        -- print("Download fehlgeschlagen!")
-    end
-end
+-- if fs.exists(name) == true then fs.delete(name) end
+-- -- -
+-- -- print("Downloading " .. name .. ".lua ...")
+-- http.request(repo .. name .. ".lua", nil, {os.time()})
+-- while true do
+--     local event, url, h = os.pullEvent()
+--     if event == "http_success" then
+--         local content = h.readAll()
+--         h.close()
+--         local file = fs.open(name, 'w')
+--         file.write(content)
+--         file.close()
+--         -- print("Saved " .. name .. ".lua")
+--         -- http.request(repo .. name .. ".lua")
+--         shell.run(name)
+--     elseif event == "http_failure" then
+--         -- print("Download fehlgeschlagen!")
+--     end
+-- end
+request = http.get(repo .. name .. ".lua")
+print(request.readAll())
+request.close()
