d87dc5fa77b5494eff96eaabf5e0663a40e7a8d9
tretapolis <MY_NAME@example.com>
2021-12-30 04:27:59 +0100

    December 30, 2021 4:27 AM

--- test.lua
+++ test.lua
@@ -1,58 +1,29 @@
-local function printUsage()
-    print("Usage:")
-    print("wget <url> <filename>")
+repo = "https://raw.githubusercontent.com/tretapolis-dev/computercraft/main/"
+name = "test"
+write("Connecting to " .. repo .. name .. ".lua ...")
+
+local ok, err = http.checkURL(repo .. name .. ".lua")
+if not ok then
+    print("Failed.")
+    if err then printError(err) end
+    return nil
 end
 
-local tArgs = {...}
-if #tArgs < 2 then
-    printUsage()
-    return
+local response = http.get(repo .. name .. ".lua", nil, true)
+if not response then
+    print("Failed.")
+    return nil
 end
 
-if not http then
-    printError("wget requires http API")
-    printError("Set http_enable to true in ComputerCraft.cfg")
-    return
-end
+print("Success.")
 
-local function get(sUrl)
-    write("Connecting to " .. sUrl .. "... ")
-
-    local ok, err = http.checkURL(sUrl)
-    if not ok then
-        print("Failed.")
-        if err then printError(err) end
-        return nil
-    end
-
-    local response = http.get(sUrl, nil, true)
-    if not response then
-        print("Failed.")
-        return nil
-    end
-
-    print("Success.")
-
-    local sResponse = response.readAll()
-    response.close()
-    return sResponse
-end
-
--- Determine file to download
-local sUrl = tArgs[1]
-local sFile = tArgs[2]
-local sPath = shell.resolve(sFile)
-if fs.exists(sPath) then
-    print("File already exists")
-    return
-end
-
--- Do the get
-local res = get(sUrl)
-if res then
+local sResponse = response.readAll()
+response.close()
+local sPath = shell.resolve(name)
+if fs.exists(sPath) then fs.delete(sPath) end
+if sResponse then
     local file = fs.open(sPath, "wb")
-    file.write(res)
+    file.write(sResponse)
     file.close()
-
-    print("Downloaded as " .. sFile)
+    print("Downloaded as " .. name)
 end
