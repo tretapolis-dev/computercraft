c1c19817f3d312460dc299fcbb0ea3cd127e1c6f
tretapolis <MY_NAME@example.com>
2021-12-30 02:24:37 +0100

    December 30, 2021 2:24 AM

--- MyGitGet_test.lua
+++ MyGitGet_test.lua
@@ -6,7 +6,7 @@ name = "test"
 if fs.exists(name) == true then fs.delete(name) end
 -- -
 -- print("Downloading " .. name .. ".lua ...")
-test = http.request(repo .. name .. ".lua")
+http.request(repo .. name .. ".lua", os.time())
 local loop = true
 event, url, h = "", "", ""
 while loop do
